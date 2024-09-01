565a3f1c1951f3dea9d2c7b7559d3f2e3fe59a66
Antonio <antoniodaleo@outlook.com>
2024-09-01 04:44:03 -0300

    Security e Form

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = [];
+    protected $helpers = ['form'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
@@ -54,5 +54,22 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
+    }
+
+    // valida se a requisição é do tipo info. post/put/delete
+    /*
+        @param string $method
+        @param boolean
+    
+    */
+    protected function checkMethod(string $method){
+        
+        $method = strtolower($method); 
+        
+        if(!$this->request->is($method)){
+            return $this->response->setStatusCode(405)->setBody('Method Not Allowed'); 
+        }
+
+        return true; 
     }
 }
--- app/Views/Back/Units/edit.php
+++ app/Views/Back/Units/edit.php
@@ -18,21 +18,50 @@
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
-   
+
     <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary"><?php echo $title ;  ?></h6>
+            <h6 class="m-0 font-weight-bold text-primary"><?php echo $title;  ?></h6>
         </div>
         <div class="card-body">
-            <div class="table-responsive">
+            <?php echo form_open(route_to('units.update', $unit->id), hidden: ['_method' => 'PUT']); ?>
 
-                
 
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="name">Nome</label>
+                    <input type="text" class="form-control" value="<?php echo old('name', $unit->name);  ?>" id="name" name="name" aria-describedby="nameHelp" placeholder="Nome">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="phone">Telefone</label>
+                    <input type="tel" class="form-control" value="<?php echo old('phone', $unit->phone);  ?>" id="phone" name="phone" aria-describedby="phoneHelp" placeholder="Telefone">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="coordinator">Coordenador</label>
+                    <input type="text" class="form-control" value="<?php echo old('coordinator', $unit->coordinator);  ?>" id="coordinator" name="coordinator" aria-describedby="coordinatorHelp" placeholder="Coordenador">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <label for="coordinator">Start Time</label>
+                    <input type="text" class="form-control" value="<?php echo old('coordinator', $unit->coordinator);  ?>" id="coordinator" name="coordinator" aria-describedby="coordinatorHelp" placeholder="Coordenador">
+                </div>
+
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
             </div>
+
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <?php echo form_close(); ?>
         </div>
     </div>
 </div>
+
+
 <?php echo $this->endSection();    ?>
 <!-- ############/.container-fluid############################ -->
 
